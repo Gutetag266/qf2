32b1a076472f086a6ed2e52132277cbf77aab1b9
Gutetag <filippietryga0@gmail.com>
2026-08-20 00:50:12 +0200

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,13 @@
+// App.tsx
+import React from 'react';
+import { QuantForgeApp } from './components/QuantForgeApp';
+
+export const App: React.FC = () => {
+  return (
+    <React.StrictMode>
+      <QuantForgeApp />
+    </React.StrictMode>
+  );
+};
+
+export default App;
